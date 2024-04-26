3060dfeaf5b6bc8e1fa0d4677654e650e8c4d876
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-04-26 10:41:32 -0400

    Initial commit

---
+++ com/example/Power.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Spencer Scarlett
+ * @version 1.0
+ * @since 2024-04-26
+ */
+
+// Power class
+public final class Power {
+
+    /** Private constructor to prevent instantiation. */
+    private Power() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
